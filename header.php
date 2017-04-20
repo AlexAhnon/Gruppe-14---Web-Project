436cbd4417b370be091538ad8ad9ed57648c7f2f
AlexAhnon <alexahnon@gmail.com>
2017-04-20 12:45:16 +0200

    Lagt til logo og tester ut en navigasjons menu

--- header.php
+++ header.php
@@ -1,1 +1,15 @@
-<a href="index.php"><div id="header">Velkommen til test websiden til gruppe 14</div></a>
+<div class="dropdown">
+	|||
+	<div class="dropdown-content">
+		<a href="kultur.php">
+			<div class="dropdown-link"></div>
+			<div class="dropdown-link"></div>
+			<div class="dropdown-link"></div>
+			<div class="dropdown-link"></div>
+		</a>
+	</div>
+</div>
+
+<a href="index.php">
+	<div id="header"></div>
+</a>
