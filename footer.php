5ee1d9462285375ee59347461a34356d7eb30109
AlexAhnon <alexahnon@gmail.com>
2017-04-27 13:37:15 +0200

    Lagt til endringer til footer

--- footer.php
+++ footer.php
@@ -1,3 +1,9 @@
 	</div>
-	<div id="footer">Campus Fjerdingen - (c) Copyright Gruppe 14 - 2017</div>
+	<div id="footer">
+		<div class="buttons-wrapper">
+			<a href="#" class="facebook">Test</a>
+			<a href="#">Test</a>
+			<a href="#">Test</a>
+		</div>
+	</div>
 </div>
