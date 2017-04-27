f4a9ade57dd089c9584640ae9949ba85a4995b4e
AlexAhnon <alexahnon@gmail.com>
2017-04-27 10:46:23 +0200

    Test side for Google maps

---
+++ test2.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html>
+	<head>
+		<title>Westerdals: Campus Fjerdingen - Test</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+	</head>
+	<body>
+		<?php require 'header.php' ?>
+
+		<iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d2000.0584897913884!2d10.75484441580288!3d59.91457663186743!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x46416e60c78540dd%3A0xa19b8032c939043a!2sCampus+Fjerdingen%2C+Chr.+Krohgs+gate+32%2C+0186+Oslo!3m2!1d59.9161644!2d10.7596752!4m5!1s0x46416e61cee4e1bf%3A0xeaab41e737da924c!2sOslo+Spektrum%2C+Sonja+Henies+plass+2%2C+0185+Oslo!3m2!1d59.912985299999995!2d10.7547424!5e0!3m2!1sen!2sno!4v1493282678112" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+		<?php require 'footer.php' ?>
+	</body>
+</html>
