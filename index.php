f1f632f2efa961f3ee07610607254bb4da111dd8
Julie Sofie <juliesofiehelmersen@gmail.com>
2017-05-11 12:21:33 +0200

    la til "viewport" i index.php for å se om det funker
    skalering

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 		<title>Westerdals: Campus Fjerdingen - Hjemmeside</title>
 		<link rel="stylesheet" type="text/css" href="css/main.css">
 		<link rel="stylesheet" type="text/css" href="css/index.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
 	</head>
 	<body>
 		<?php require 'header.php' ?>
