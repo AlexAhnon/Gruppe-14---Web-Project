17d1a6474f713983a8461962c491d01dcd327d74
AlexAhnon <alexahnon@gmail.com>
2017-05-22 12:49:26 +0200

    La til tekst og formatering til om-siden

--- om-siden.php
+++ om-siden.php
@@ -10,7 +10,20 @@
 		<?php require 'header.php' ?>
 				
 		<!-- Start av content -->
-		Hei.
+		<h1>Om siden</h1>
+		<p style="width:800px; margin: auto;">
+		Hovedmålet med denne siden var å skape ett prosjekt som gjorde det enkelt for studenter å finne fram til forskjellige steder rundt nærmiljøet til Campus Fjerdingen. Det finnes 4 forskjellige kategorier hvor man kan finne en rekke med relaterte aktiviteter, og inneholder addresser, veibeskrivelser og lenker til hjemmesider, alt som er koblet opp mot en database som styrer alt.<br><br>
+
+		Alle logoer og bilder tilhører <a href="http://westerdals.no/">Westerdals ACT</a> og er kun brukt for design av siden. Websiden er brukt som utdanningsmaterialle for PRO101-1 17V Webprosjekt.<br><br>
+
+		<h2>Websiden var designet og laget av følgende studenter:</h1>
+		<ul style="list-style: none;">
+			<li>Alex Ahnon Hansen</li>
+			<li>Flatemo Sigurd</li>
+			<li>Marcus Kongevold</li>
+			<li>Ahmed Muhammad</li>
+			<li>Julie Sofie Helmersen</li>
+		</ul></p>
 
 		<?php require 'footer.php' ?>
 	</body>
