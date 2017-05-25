be43599dbfbac944ed8271d2149437033d532cca
Alex Ahnon Hansen <alexahnon@gmail.com>
2017-05-25 14:28:52 +0200

    Lagt til forstørrelses knapp med hjelpetekst

--- footer.php
+++ footer.php
@@ -1,5 +1,11 @@
 <!-- Copyright og ekstene lenker til Westerdals sine sosiale medier -->
 <div id="footer">
+	<div class="zoom">
+		<div class="zoom-hover-div">
+			Hold CTRL tasten nede. Trykk på + for å forstørre eller - for å forminske.
+		</div>
+		Skriftstørrelse
+	</div>
 	© Westerdals ACT og Gruppe 14 fra Webprosjekt
 	<a href="https://www.facebook.com/westerdalsosloact/" target="_blank"><img class="facebook" src="img/face.png" alt="Facebook"></a>
 	<a href="https://www.instagram.com/westerdalsact/?hl=en" target="_blank"><img class="instagram" src="img/instagram.jpg" alt="Instragram"></a>
